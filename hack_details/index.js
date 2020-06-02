36915d6302681bf6210704f3fb664d0a6e3e0e6c
Yash Singhal <singhalyash8080>
2020-06-02 14:20:06 +0530

    added pages

---
+++ hack_details/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+var hack_name="VIT Hack"
+var date='18-03-2020'
+var venue='Technology tower,Vit Vellore'
+var desc = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident libero perspiciatis veniam magni tempore distinctio quae impedit voluptatem voluptatum nihil cupiditate nobis quis, quo veritatis inventore repellat, nesciunt sapiente atque.'
+var link = '<a href="#">this is a dead link </a>'
+
+$('#name').append(hack_name)
+$('#date').append(date)
+$('#Venue').append(venue)
+$('#desc').append(desc)
+$('#link').append(link)
