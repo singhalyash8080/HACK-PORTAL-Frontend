d35b8a13f85748bf1d8ce2d86cb0c2a8e17b5557
Yash Singhal <singhalyash8080@gmail.com>
2020-08-24 13:22:45 +0530

    changed path2

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ var port = process.env.PORT || 8080
 app.use(express.static(__dirname))
 
 app.get('/',function(req,res){
-
-    res.render('index.html')
+    res.render('./index.html')
 })
 
 app.listen(port, function(){
